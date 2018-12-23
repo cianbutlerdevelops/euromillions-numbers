80d6f6428c5232bca961e7ec0bfd7031d952e9ca
Cian Butler <cianbutlerdevelops@gmail.com>
2018-12-23 15:06:53 +0000

    functions created

---
+++ app.js
@@ -0,0 +1,18 @@
+let mainNumbers = () => {
+    let main = Math.floor(Math.random() * 50 + 1);
+    console.log('Your Main Numbers are : ' +main);   
+}
+
+mainNumbers();
+mainNumbers();
+mainNumbers();
+mainNumbers();
+mainNumbers();
+
+let bonusNumbers = () => {
+    let bonus = Math.floor(Math.random()* 12 + 1);
+    console.log('Your Bonus Numbers are ' + bonus);
+}
+
+bonusNumbers();
+bonusNumbers();
